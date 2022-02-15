798763f3c20d7d5c91cfd11cc45407769bb797c9
Johnexzy <obajohn75@gmail.com>
2022-02-15 07:43:58 +0100

    feat: added balabceof address

--- index.js
+++ index.js
@@ -14,7 +14,21 @@ let options = {
   fromBlock: 0,                  //Number || "earliest" || "pending" || "latest"
   toBlock: 'latest'
 };
+/**
+ * Get all AfroList mints
+ * @returns Array
+ */
 const ShowMints = async ()=> await myContract.getPastEvents('OGMinted', options)
+const balanceOf = async (address)=> await myContract.methods.balanceOf(address).call()
+const processBalance = async (msg)=>{
+  const address = msg.content.trim.split(' ')[1]
+  if (Web3.utils.isAddress(address)) {
+    msg.reply(await balanceOf(address))
+  }
+  else{
+    msg.reply(`${address} is not valid`)
+  }
+}
     // .catch(err => throw err);
 web3.eth.getBlockNumber().then((result) => {
   console.log("Latest Ethereum Block is ",result);
@@ -26,25 +40,45 @@ client.on("ready", () => {
 
 client.on("message", async (msg) => {
   if (msg.content.startsWith("!token-")) {
-    const id = parseInt(msg.content.split('-')[1])
-    const mints = await ShowMints()
-    if(id > (mints.length + 7) - 1) {
-      msg.reply('token does not exist')
-      return;
-    }
-    msg.reply(`Fetching token ${id}...`)
-    await axios.get('https://api.afroapes.com/v1/collections/afro-apes-the-origin/'+id).then(response => {
-    
-      const url = response.data
-      msg.reply(url.temp_path);
-      // msg.channel.send(new MessageAttachment(url.temp_path))
-    })
+    await getToken(msg)
     // msg.reply(mints[id]);
   }
-  if (msg.content === "!mints") {
+  else if (msg.content === "!mints") {
     const mints = await ShowMints()
     msg.reply(`AL mint ${mints.length}/50`);
   }
+  else if (msg.content.startsWith('!balance') ) {
+    await processBalance(msg)
+  }
 });
 
+/**
+ * 
+ */
+client.on('message', async (msg) => {
+  if (msg.content = "substi") {
+    await getToken(msg)
+    // msg.reply(mints[id]);
+  }
+})
+/**
+ * Get a token
+ * @param {Object} msg 
+ * @returns void
+ */
+const getToken = async (msg) => {
+  const id = parseInt(msg.content.split('-')[1])
+  const mints = await ShowMints()
+  if(id > (mints.length + 7) - 1) {
+    msg.reply('token does not exist')
+    return;
+  }
+  msg.reply(`Fetching token ${id}...`)
+  await axios.get('https://api.afroapes.com/v1/collections/afro-apes-the-origin/'+id).then(response => {
+  
+    const url = response.data
+    msg.reply(url.temp_path);
+    // msg.channel.send(new MessageAttachment(url.temp_path))
+  })
+}
 client.login(process.env.CLIENT_TOKEN); //login bot using token
